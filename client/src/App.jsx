9d524a794978581005675f0f0fe48f5e5ef24a67
psychic-coder <rohitganguly450@gmail.com>
2024-05-23 08:07:17 +0530

    signup/signin page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{lazy} from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
+const Home=lazy(()=>import("./pages/Home"))
+const Login=lazy(()=>import("./pages/Login"))
+const Groups=lazy(()=>import("./pages/Groups"))
+const Chat=lazy(()=>import("./pages/Chat"))
+
+
 
 function App() {
   return (
-    <div >App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />   
+        <Route path="/groups" element={<Groups/>} />  
+        <Route path="/chat/:chatId" element={<Chat/>} />  
+        <Route path="/login" element={<Login/>} />  
+      </Routes>
+    </BrowserRouter>
   )
 }
+
+/*Performance Optimization: By loading components only when needed, you reduce the initial load time and improve the performance of your application.
+Code Splitting: It helps in splitting the code into smaller chunks, which can be loaded on demand.
+Improved User Experience: Users can see the initial content faster, and subsequent content is loaded as needed.*/ 
 
 export default App
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,203 @@
+import {
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import {CameraAlt as CameraAltIcon} from "@mui/icons-material"
+import { VisuallyHiddenInput } from "../components/styles/StyledComponent";
+import {useInputValidation} from "6pp";
+
+
+function Login() {
+    
+  const [isLogin, setIsLogin] = useState(true);
+  const toggleLogin = () => setIsLogin((prev) => !prev);
+
+
+  //the useInputValidation is similar to the useState hook
+  const name= useInputValidation("");
+  const bio= useInputValidation("");
+  const username= useInputValidation("");
+  const password= useInputValidation("");
+  return (
+    <Container
+      component={"main"}
+      maxWidth="xs"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {isLogin ? (
+          <>
+            <Typography variant="h5">Login</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+            >
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+                value={username.value}
+                onChange={username.changeHandler}
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+                value={password.value}
+                onChange={password.changeHandler}
+              />
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+              >
+                Login
+              </Button>
+              <Typography textAlign={"center"} m={"1rem"}>
+                OR
+              </Typography>
+              <Button
+                variant="text"
+                color="secondary"
+                onClick={toggleLogin}
+                fullWidth
+              >
+                Sign Up Instead
+              </Button>
+            </form>
+          </>
+        ) : (
+          <>
+            <Typography variant="h5">Sign Up</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+            >
+              <Stack position={"relative"} width={"10rem"} margin={"auto"}>
+                <Avatar
+                  sx={{
+                    width: "10rem",
+                    height: "10rem",
+                    objectFit: "contain",
+                  }}
+                />
+                <IconButton
+                sx={{
+                    position:"absolute",
+                    bottom:"0",
+                    right:"0",
+                    colo:"white",
+                    bgcolor:"rgba(0,0,0,0.5)",
+                    ":hover":{
+                        bgcolor:"rgba(0,0,0,0.7)"
+                    }
+                }}
+                component="label"
+                >
+                    <>
+                    <CameraAltIcon/>
+                    <VisuallyHiddenInput type="file" />
+                    </>
+                </IconButton>
+              </Stack>
+              <TextField
+                required
+                fullWidth
+                label="Name"
+                margin="normal"
+                variant="outlined"
+                value={name.value}
+                onChange={name.changeHandler}
+              />
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+                value={username.value}
+                onChange={username.changeHandler}
+              />
+              <TextField
+                required
+                fullWidth
+                label="Bio"
+                margin="normal"
+                variant="outlined"
+                value={bio.value}
+                onChange={bio.changeHandler}
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+                value={password.value}
+                onChange={password.changeHandler}
+              />
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+              >
+                Sign Up
+              </Button>
+              <Typography textAlign={"center"} m={"1rem"}>
+                OR
+              </Typography>
+              <Button
+                variant="text"
+                color="secondary"
+                onClick={toggleLogin}
+                fullWidth
+              >
+                Login Instead
+              </Button>
+            </form>
+          </>
+        )}
+      </Paper>
+    </Container>
+  );
+}
+
+export default Login;
