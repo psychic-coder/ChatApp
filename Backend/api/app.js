30a1f6daef60e303ca06c48a0d0d00ab0d0a019e
psychic-coder <rohitganguly450@gmail.com>
2024-05-27 12:29:17 +0530

    started with socket.js

--- Backend/api/app.js
+++ Backend/api/app.js
@@ -2,61 +2,130 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import userRoute from "../api/routes/user.routes.js"
-import chatRoute from "../api/routes/chat.routes.js"
-import adminRoute from "../api/routes/admin.routes.js"
+import userRoute from "../api/routes/user.routes.js";
+import chatRoute from "../api/routes/chat.routes.js";
+import adminRoute from "../api/routes/admin.routes.js";
 import { connectDB } from "../api/utils/features.js";
-
-
+import cors from "cors";
+import { Server } from "socket.io";
 import dotenv from "dotenv";
 import { errorMiddleware } from "./middlewares/error.js";
 import { createMessagesInAChat } from "./seeders/chat.js";
-
-
-
-
+import { createServer } from "http";
+import { NEW_MESSAGE, NEW_MESSAGE_ALERT } from "./constants/events.js";
+import { v4 as uuid} from "uuid"
+import { getSockets } from "./lib/helper.js";
+import { Message } from "./models/message.js";
 
 dotenv.config();
+
+
+const userSocketIDs=new Map();
+
 //mongoURI=process.env.MONGO_URI;
 connectDB("mongodb://localhost:27017/ChatterBox");
 
 var app = express();
-
-app.use(logger('dev'));
+const server = createServer(app);
+app.use(logger("dev"));
+//we did cross origin as our users are gonna be from different platforms
+app.use(cors());
 //express.json is used to access the json data from the frontend
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
 app.use(express.json());
 //express.urlencoded is used when we send form data from the frontend
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // createSingleChats(10);
 // createGroupChats(10);
 //createMessagesInAChat("6652fad5328fb55d8d43b9dc",50)
 
+app.use("/user", userRoute);
+app.use("/chat", chatRoute);
+app.use("/admin", adminRoute);
 
-app.use("/user", userRoute)
-app.use("/chat", chatRoute)
-app.use("/admin", adminRoute)
+app.get("/", (req, res) => {
+  res.send("Hello  WORLD !");
+});
 
-app.get("/",(req,res)=>{
-  res.send("Hello  WORLD !")
+//we creating a socket middleware overhere
+io.use((socket,next)=>{
+
 })
 
+io.on("connection",(socket)=>{
+  const user={
+    _id:"hgsv",
+    name:"vs"
+  }
 
+  //we are mapping the value of user._id to socket.id,which means that the user with user._id is connected to that particular socket.id
+  userSocketIDs.set(user._id.toString(),socket.id)
 
-app.use(errorMiddleware)
+  console.log(userSocketIDs);
+  //console.log("A user connected ",socket.id);
 
-export const adminSecretKey=process.env.ADMIN_SECRET_KEY||"rohitIeeeCs96"
+  socket.on(NEW_MESSAGE, async ({chatId,members,message})=>{
 
-const port =5000;
-export const envMode=process.env.NODE_ENV.trim()||"PRODUCTION";
-app.listen(port, () => {
-    console.log(`Server is running on port ${port} in ${envMode} mode `);
+    const messageForRealTime={
+      content:message,
+      _id:uuid(),
+      sender:{
+        _id:user._id,
+        name:user.name
+      },
+      chat:chatId,
+      createdAt:new Date().toISOString(),
+    }
+
+    const messageForDB={
+      content:message,
+      sender:user._id,
+      chat:chatId
+    }
+      const membersSocket=getSockets(members)
+
+      io.to(membersSocket).emit(NEW_MESSAGE,{
+        chatId,
+        message:messageForRealTime
+      })
+
+      io.to(membersSocket).emit(NEW_MESSAGE_ALERT,{chatId});
+
+     try {
+      await Message.create(messageForDB);
+     } catch (error) {
+      console.log(error)
+     }
   });
 
+  
 
+  socket.on("disconnect",()=>{
+    console.log("User disconnected ")
+    userSocketIDs.delete(user._id.toString())
+  })
+})
 
+app.use(errorMiddleware);
 
+export const adminSecretKey = process.env.ADMIN_SECRET_KEY || "rohitIeeeCs96";
+
+const port = 5000;
+export const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
+server.listen(port, () => {
+  console.log(`Server is running on port ${port} in ${envMode} mode `);
+});
 
 export default app;
+
+export {userSocketIDs}
--- Backend/api/lib/helper.js
+++ Backend/api/lib/helper.js
@@ -1,2 +1,10 @@
+import { userSocketIDs } from "../app.js";
+
 export const getOtherMember = (members, userId) =>
   members.find((member) => member._id.toString() !== userId.toString());
+
+export const getSockets = (users = []) => {
+  //we give the id of the users and get hold of the values of the sockets corresponding to that particular user from tge userSocketIDs and then we return the socket array
+  const sockets = users.map((user) => userSocketIDs.get(user._id.toString()));
+  return sockets;
+};
